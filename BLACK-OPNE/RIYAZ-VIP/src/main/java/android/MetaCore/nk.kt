86026e19d9966320019ddedff17279ee811e33d4
JITENDRA YADAV <106445997+ZayYadav@users.noreply.github.com>
2026-08-17 15:09:25 +0530

    Update nk.kt

--- BLACK-OPNE/RIYAZ-VIP/src/main/java/android/MetaCore/nk.kt
+++ BLACK-OPNE/RIYAZ-VIP/src/main/java/android/MetaCore/nk.kt
@@ -21,7 +21,7 @@ class nk {
         var Msg: String = "Ready"
 
         const val PREFERENCE_NAME: String = "license_cache"
-        var ActivationUrl: String = "https://parallaxserver.online/SDK/connect.php"
+        var ActivationUrl: String = "https://parallaxloadersdk.parallaxserver.online/connect.php"
 
         @JvmStatic
         fun getActivatedSdk(): Boolean {
@@ -151,7 +151,7 @@ class nk {
 
         @JvmStatic
         fun 获取接口地址(): String {
-            return "https://parallaxserver.online/SDK/connect.php"
+            return "https://parallaxloadersdk.parallaxserver.online/connect.php"
         }
         
         @JvmStatic
