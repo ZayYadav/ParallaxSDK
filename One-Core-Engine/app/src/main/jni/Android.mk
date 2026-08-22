d66d49cbc5a89bf0b71480802d869e850b773bbd
JITENDRA YADAV <106445997+ZayYadav@users.noreply.github.com>
2026-08-22 23:22:26 +0530

    Compile process-bound APK guard

--- One-Core-Engine/app/src/main/jni/Android.mk
+++ One-Core-Engine/app/src/main/jni/Android.mk
@@ -21,7 +21,8 @@ LOCAL_MODULE := ParallaxLoader
 
 LOCAL_SRC_FILES := main.cpp \
     NativeApkAttestation.cpp \
-    NativeLicenseAttestationBridge.cpp
+    NativeLicenseAttestationBridge.cpp \
+    ProcessBindingGuard.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/backends/external/curl-android-$(TARGET_ARCH_ABI)/include
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/backends/external/openssl-android-$(TARGET_ARCH_ABI)/include
