b190bbce1cadf0f3bc0da0eec8a0c05ed96eb65b
JITENDRA YADAV <106445997+ZayYadav@users.noreply.github.com>
2026-08-22 17:07:21 +0530

    Move game package strings into native AES vault

--- One-Core-Engine/app/src/main/java/com/onecore/loader/Config.java
+++ One-Core-Engine/app/src/main/java/com/onecore/loader/Config.java
@@ -1,10 +1,11 @@
 package com.onecore.loader;
 
+import com.onecore.loader.security.NativeStringVault;
 import org.lsposed.lsparanoid.Obfuscate;
 
 @Obfuscate
 public class Config {
-    
-    public static final String[] GAME_LIST_PKG = {"com.pubg.imobile", "com.tencent.ig", "com.pubg.krmobile"};
+
+    public static final String[] GAME_LIST_PKG = NativeStringVault.gamePackages();
     public static final int USER_ID = 0;
 }
