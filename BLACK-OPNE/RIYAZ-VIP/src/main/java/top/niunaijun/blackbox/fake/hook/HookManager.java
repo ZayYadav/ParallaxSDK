4308088a9833b29646be107df4abade1c9e59d47
JITENDRA YADAV <106445997+ZayYadav@users.noreply.github.com>
2026-08-31 23:58:37 +0530

    fix(auth): enable Facebook web-only package resolver

--- BLACK-OPNE/RIYAZ-VIP/src/main/java/top/niunaijun/blackbox/fake/hook/HookManager.java
+++ BLACK-OPNE/RIYAZ-VIP/src/main/java/top/niunaijun/blackbox/fake/hook/HookManager.java
@@ -37,7 +37,7 @@ public class HookManager {
             addInjector(new IDisplayManagerProxy());
             addInjector(new IJobServiceProxy());
             addInjector(new IActivityManagerProxy());
-            addInjector(new IPackageManagerProxy());
+            addInjector(new IFacebookWebPackageManagerProxy());
             addInjector(new ITelephonyManagerProxy());
             addInjector(new HCallbackStub());
             addInjector(new IWifiManagerProxy());
