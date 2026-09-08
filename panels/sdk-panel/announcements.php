1e692939cc80ac7bbea00c86a69496c658127245
JITENDRA YADAV <106445997+ZayYadav@users.noreply.github.com>
2026-09-08 16:54:14 +0530

    Apply SDK panel UI V2 to announcements.php

--- panels/sdk-panel/announcements.php
+++ panels/sdk-panel/announcements.php
@@ -346,6 +346,7 @@ header {
 .form-select option { background:#1e2a45; color:#fff; }
 
 </style>
+<link rel="stylesheet" href="ui_v2.css?v=20260908">
 </head>
 <body>
 <div class="bg-orbs"><div class="bg-orb ob1"></div><div class="bg-orb ob2"></div><div class="bg-orb ob3"></div></div>
